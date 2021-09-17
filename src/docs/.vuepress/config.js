929c35e6138cf0d46c45f4285e1994b6ab0860d0
zhaozhou <zhou773@foxmail.com>
2021-09-17 11:33:15 +0800

    基本Sql语句集合

--- src/docs/.vuepress/config.js
+++ src/docs/.vuepress/config.js
@@ -101,7 +101,7 @@ module.exports = {
                 }, {
                     title: 'MySQL',
                     children: [
-                        '/java/3/', '/java/3/1', '/java/3/2', '/java/3/3', '/java/3/4', '/java/3/5', '/java/3/6', '/java/3/7'
+                        '/java/3/', '/java/3/1', '/java/3/2', '/java/3/3', '/java/3/4', '/java/3/5', '/java/3/6', '/java/3/7', '/java/3/8'
                     ]
                 },
                 {
